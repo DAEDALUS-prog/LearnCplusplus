1fb87e158216ae53acc5140de5189b7bd7d506ea
DAEDALUS <69119928+DAEDALUS-prog@users.noreply.github.com>
2021-09-04 00:13:32 +0300

    Converting miles and feet to kilometers and meters

---
+++ mileInKm/mileInkm.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	const int ftInMile = 5280;
+	const double kmInMile = 1.609344;
+	double dMile, dFt, dKm, dM;
+	
+	cout << "Distance in miles and feet" << endl;
+
+	cout << "Miles: " << endl;
+	cin >> dMile;
+
+	cout << "Feet: " << endl;
+	cin >> dFt;
+
+	double distMile = dMile + (double)dFt / ftInMile;
+	double distKm = distMile * kmInMile;
+
+	dKm = (int)distKm;
+	dM = (int)((distKm-dKm)*1000);
+
+	cout << "Distance in kilometers and meters" << endl;
+	cout << "Kilometers: " << dKm << endl;
+	cout << "Meters: " << dM << endl;
+
+	return 0;
+}
