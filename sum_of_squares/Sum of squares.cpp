b832c9f6e7c65f6c2c99ea73edc5b93ee51645da
DAEDALUS <69119928+DAEDALUS-prog@users.noreply.github.com>
2021-09-04 22:24:12 +0300

    Add files via upload

---
+++ sum_of_squares/Sum of squares.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum_of_squares(int value)
+{
+    int sum = 0, copy_value = value;
+    
+
+    while (value) {
+        sum = sum + value * value;
+        value--;
+    }
+
+    cout << "Sum of squares from 1 to " << copy_value << ": " << sum << endl;
+    return 0;
+}
+
+int check_value(int value)
+{
+    if (value > 0) {
+        return value;
+    }
+
+    else {
+        return 0;
+    }
+}
+
+
+int main() {
+    cout << "Enter the value > 0: " << endl;
+
+    int value;
+    cin >> value;
+
+    value = check_value(value);
+    sum_of_squares(value);
+
+    return 0;
+}
